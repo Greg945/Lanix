cf0c3a59da961bfdda5d2dcf46186a5643e11950
Greg945 <xtimsandx@gmail.com>
2025-10-13 20:18:30 +0200

    icon

--- lanix_app.py
+++ lanix_app.py
@@ -15,6 +15,18 @@ import io
 from pathlib import Path
 import sys
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
 # valueeee = browser_detection_engine()
 # st.write(valueeee)
 
